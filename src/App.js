645e986600cbf39806dde62c373381c1629e196e
David Ogbeide <boyowao@gmail.com>
2023-02-07 18:44:23 -0700

    init category list

--- src/App.js
+++ src/App.js
@@ -1,7 +1,42 @@
+import './categories.scss'
 
 const App = () => {
+
+  const categories = [
+    {
+      id: 1,
+      title: 'Art'
+    },
+    {
+      id: 2,
+      title: 'Jewelry'
+    },
+    {
+      id: 3,
+      title: 'Food'
+    },
+    {
+      id: 4,
+      title: 'Clothing'
+    },
+    {
+      id: 5,
+      title: 'Astrology'
+    },
+  ]
+
   return (
-    <div>Nature's Delight Magic</div>
+    <div className="categories">
+      {categories.map(({id, title}) => (
+        <div className="category" key={id}>
+          <img />
+          <div className="category-body">
+            <h2>{title}</h2>
+            <p>Browse</p>
+          </div>
+        </div>
+      ))}
+    </div>
   );
 }
 
